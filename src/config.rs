6447ae1a4d5613e1db72609ffd7bccbb704ab952
jonasen1988 <131840919+jonasen1988@users.noreply.github.com>
2025-05-16 13:48:59 +0800

    更换

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["35.220.169.93"];
+pub const RS_PUB_KEY: &str = "JzYIgTGxTifL+2b4ecKiAH7fPdXBRpycG3TZfzanUSw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
